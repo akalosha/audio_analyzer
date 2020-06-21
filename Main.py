fac13eb812e9cc654a98ba329d87777d61cb19c7
airesearch89 <andreikalosha@mail.ru>
2020-06-21 15:11:24 +0300

    Change method to spectrogram

--- Main.py
+++ Main.py
@@ -1,108 +1,94 @@
+import csv
+import pathlib
+import warnings
+
+import librosa
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from keras import layers
+from keras.models import Sequential
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+warnings.filterwarnings('ignore')
+
+
+
+cmap = plt.get_cmap('inferno')
+plt.figure(figsize=(8,8))
+genres = '1 0'.split()
+
+
+def made_vectors(audio_folder_name, image_folder_name, csv_folder_name):
+    for g in genres:
+        pathlib.Path(f'img_data/{g}').mkdir(parents=True, exist_ok=True)
+        for filename in os.listdir(f'./mp3/{g}'):
+            songname = f'./mp3/{g}/{filename}'
+            y, sr = librosa.load(songname, mono=True, duration=5)
+            plt.specgram(y, NFFT=2048, Fs=2, Fc=0, noverlap=128, cmap=cmap, sides='default', mode='default',
+                         scale='dB');
+            plt.axis('off');
+            plt.savefig(f'img_data/{g}/{filename[:-3].replace(".", "")}.png')
+            plt.clf()
+    header = 'filename chroma_stft rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
+    for i in range(1, 21):
+        header += f' mfcc{i}'
+    header += ' label'
+    header = header.split()
+    file = open('dataset.csv', 'w', newline='')
+    with file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+    for g in genres:
+        for filename in os.listdir(f'./mp3/{g}'):
+            songname = f'./mp3/{g}/{filename}'
+            y, sr = librosa.load(songname, mono=True, duration=30)
+            rmse = librosa.feature.rmse(y=y)
+            chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+            spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+            spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+            rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+            zcr = librosa.feature.zero_crossing_rate(y)
+            mfcc = librosa.feature.mfcc(y=y, sr=sr)
+            to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'
+            for e in mfcc:
+                to_append += f' {np.mean(e)}'
+            to_append += f' {g}'
+            file = open('dataset.csv', 'a', newline='')
+            with file:
+                writer = csv.writer(file)
+                writer.writerow(to_append.split())
+    data = pd.read_csv('dataset.csv')
+    data.head()
+    # Удаление ненужных столбцов
+    data = data.drop(['filename'], axis=1)
+    # Создание меток
+    genre_list = data.iloc[:, -1]
+    encoder = LabelEncoder()
+    y = encoder.fit_transform(genre_list)
+    # Масштабирование столбцов признаков
+    scaler = StandardScaler()
+    X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype=float))
+    return X, y
+
+
+X_train, y_train = made_vectors('mp3', 'img_data', 'dataset.csv')
+
+
+model = Sequential()
+model.add(layers.Dense(256, activation='relu', input_shape=(X_train.shape[1],)))
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 import os
 
-import numpy as np
-import pydub
-from tensorflow import keras
-from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
+classifier = model.fit(X_train, y_train, epochs=20)
 
-
-def read(f, normalized=False):
-    """MP3 to numpy array"""
-    a = pydub.AudioSegment.from_mp3(f)
-    y = np.array(a.get_array_of_samples())
-    if a.channels == 2:
-        y = y.reshape((-1, 2))
-    if normalized:
-        return a.frame_rate, np.float32(y) / 2 ** 15
-    else:
-        return a.frame_rate, y
-
-
-def to_categorical(vector, nb_classes):
-    if not nb_classes:
-        nb_classes = np.max(vector) + 1
-    Y = np.zeros((len(vector), nb_classes))
-    for id, item in enumerate(vector):
-        Y[id][0] = vector[id]
-        Y[id][1] = 1 - vector[id]
-    return Y
-
-
-def addVectorsToTest(x, y, vector, res):
-    addVector(res, x, np.array(vector[:, 0], dtype=np.float32).reshape(660096, ), y)
-    addVector(res, x, np.array(vector[:, 1], dtype=np.float32).reshape(660096, ), y)
-
-
-def addVector(anwser, x_result, vector, y_result):
-    for i in range(0, last_index, count_shags):
-        x_result.append(vector[i:i + shag])
-        y_result.append(anwser)
-
-
-def readFolder(folder_name, x, y, answer):
-    for root, dirs, files in os.walk(folder_name):
-        for filename in files:
-            sr, vector = read(folder_name + '/' + filename)
-            vector = (vector + 32768) / 65535
-            if np.array(vector).shape[0] == 660096:
-                addVectorsToTest(x, y, vector, answer)
-
-
-full_len = 660096
-len_kyrs = 2
-
-shag = (int)(full_len / 5)
-last_index = (int)(full_len - shag)
-count_shags = (int)(full_len / 150)
-n = shag
-m = len_kyrs
-r = pow(n / m, 1 / 3)
-k1 = (int)(m * pow(r, 2))
-k2 = (int)(m * r)
-
-x_train = []
-y_train = []
-
-x_test = []
-y_test = []
-
-readFolder("./0", x_train, y_train, 0)
-readFolder("./1", x_train, y_train, 1)
-readFolder("./05", x_train, y_train, 0.5)
-
-readFolder("./0t", x_test, y_test, 0)
-readFolder("./1t", x_test, y_test, 1)
-
-y_train = np.array(y_train, dtype=np.float32)
-y_test = np.array(y_test, dtype=np.float32)
-x_train = np.array(x_train)
-x_test = np.array(x_test)
-
-y_train = to_categorical(y_train, 2)
-y_test = to_categorical(y_test, 2)
-
-def build_model(learning_rate=0.00001):
-    model = keras.Sequential()
-    model.add(Dense(1000, activation='relu', input_shape=(shag,), kernel_regularizer=keras.regularizers.l1(0.01),
-    activity_regularizer=keras.regularizers.l2(0.3)))
-    model.add(BatchNormalization())
-    model.add(Dropout(0.5))
-    model.add(Dense(k2, activation='relu',kernel_regularizer=keras.regularizers.l1(0.01),
-    activity_regularizer=keras.regularizers.l2(0.3)))
-    model.add(BatchNormalization())
-    model.add(Dropout(0.5))
-    model.add(Dense(len_kyrs, activation='softmax'))
-    return model
-
-
-model = build_model()
-
-loss_fn = keras.losses.SparseCategoricalCrossentropy()
-opt = keras.optimizers.Adam(learning_rate=0.00001)
-model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
-model.fit(x_train, y_train, batch_size=500, epochs=1)
-
-result = model.predict(x_test)
-
-_, accuracy = model.evaluate(x_test, y_test)
-print('Accuracy: %.2f' % (accuracy * 100))
+# result = model.predict(X_test)
+#
+# _, accuracy = model.evaluate(X_test, y_test)
+# print('Accuracy: %.2f' % (accuracy * 100))
+# y=2
